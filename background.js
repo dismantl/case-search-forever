992e86c10f49c88c3600393b7a2be2a861bc2907
Dan Staples <dan@acab.enterprises>
2021-07-09 17:45:35 -0400

    Remove existing session cookie before setting new one

--- background.js
+++ background.js
@@ -1,14 +1,22 @@
 chrome.alarms.create("alarm", { periodInMinutes: 1 });
 function fetch_session(alarm) {
-    fetch('https://api.acab.enterprises/mjcsproxy/session')
-    .then(response => response.text())
-    .then(session_id => {
-        chrome.cookies.set({
-            domain: 'casesearch.courts.state.md.us',
-            name: 'JSESSIONID',
-            path: '/',
-            url: 'https://casesearch.courts.state.md.us/casesearch/processDisclaimer.jis',
-            value: session_id
+    chrome.cookies.remove({
+        url: 'https://casesearch.courts.state.md.us/',
+        name: 'JSESSIONID'
+    })
+    .then(details => {
+        fetch('https://api.acab.enterprises/mjcsproxy/session')
+        .then(response => response.text())
+        .then(session_id => {
+            chrome.cookies.set({
+                domain: 'casesearch.courts.state.md.us',
+                name: 'JSESSIONID',
+                path: '/',
+                httpOnly: true,
+                url: 'https://casesearch.courts.state.md.us/casesearch/processDisclaimer.jis',
+                value: session_id
+            });
+            console.log(session_id);
         });
     });
 }
