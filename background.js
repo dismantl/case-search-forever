7c3d387aafbd768abb8394c73e4a2f9b3f014290
Dan Staples <dan@acab.enterprises>
2021-07-21 15:31:40 -0400

    Added redirect from disclaimer page to search form

--- background.js
+++ background.js
@@ -5,6 +5,10 @@ function fetch_session(alarm) {
         name: 'JSESSIONID'
     })
     .then(details => {
+        // MJCS doesn't return an Access-Control-Allow-Origin header, so CORS prevents the 
+        // extension service worker from reading session cookies fetched directly from MJCS.
+        // This is a change since manifest v3, as service workers, unlike background pages,
+        // don't have access to the DOM. So we use a custom proxy server as a workaround.
         fetch('https://api.acab.enterprises/mjcsproxy/session')
         .then(response => response.text())
         .then(session_id => {
