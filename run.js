e2896e4bab87668b0bef5b9bf3b071eb7a3f8a15
Dan Staples <dan@acab.enterprises>
2022-04-28 11:51:22 -0400

    Remove CAPTCHA widget

--- run.js
+++ run.js
@@ -1,4 +1,5 @@
 { // Block used to avoid setting global variables
     document.querySelector('input[name="searchTrialPersonAction"]').removeAttribute("disabled");
     document.querySelector('input[name="searchTrialCaseNumAction"]').removeAttribute("disabled");
+    document.getElementById('recaptchaWidget').remove();
 }
