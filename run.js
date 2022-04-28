9c87a306ef2789e46e72135d64a295f1f825a642
Dan Staples <dan@acab.enterprises>
2022-04-28 11:49:28 -0400

    Enable both search buttons

--- run.js
+++ run.js
@@ -1,4 +1,4 @@
 { // Block used to avoid setting global variables
-    const el = document.querySelector('input[name="searchTrialPersonAction"]')
-    el.removeAttribute("disabled");
+    document.querySelector('input[name="searchTrialPersonAction"]').removeAttribute("disabled");
+    document.querySelector('input[name="searchTrialCaseNumAction"]').removeAttribute("disabled");
 }
