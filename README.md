072b092035bd9d6c5af8fe246e48608db2ca8972
Dan Staples <dan@acab.enterprises>
2022-05-04 10:21:33 -0400

    Fixes and added bypass for disclaimer page

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Case Search Forever
 Bypass that goddamn CAPTCHA on the Maryland Judiciary Case Search.
 
-This is a minimalist Chrome extension that allows you to use the Maryland Judiciary Case Search without having to click through the annoying CAPTCHA.
+This is a minimalist Chrome extension that allows you to use the Maryland Judiciary Case Search without having to click through the annoying CAPTCHA or disclaimer page.
--- run.js
+++ run.js
@@ -1,5 +1,13 @@
-{ // Block used to avoid setting global variables
-    document.querySelector('input[name="searchTrialPersonAction"]').removeAttribute("disabled");
-    document.querySelector('input[name="searchTrialCaseNumAction"]').removeAttribute("disabled");
-    document.getElementById('recaptchaWidget').remove();
+{
+    var recaptcha = document.getElementById('recaptchaWidget');
+    if (recaptcha !== null) {
+        document.querySelector('input[name="searchTrialPersonAction"]').removeAttribute("disabled");
+        document.querySelector('input[name="searchTrialCaseNumAction"]').removeAttribute("disabled");
+        recaptcha.remove();
+    }
+    var disclaimer = document.querySelector('input[name="disclaimer"]');
+    if (disclaimer !== null) {
+        disclaimer.checked = true;
+        document.getElementById('frmCasesearchdisclaimer').submit();
+    }
 }
